5e34877d1fdb9a637045e012576bc56e23a9911a
allivka <allivka1920@gmail.com>
2025-11-29 22:11:13 +0300

    Implemented the basic functionality

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 A simple implementation of vislib gyro interfaces for mpu6050 gyroscope sensor with electronic cats library under the hood
 
-## version: 0.0.0
+## version: 0.0.1
 
 ## installation
 
--- include/mpu6050.hpp
+++ include/mpu6050.hpp
@@ -1,1 +1,47 @@
 #pragma once
+
+#include <vislib.hpp>
+#include <../MPU6050/src/MPU6050.h>
+
+namespace vislib_mpu6050 {
+    namespace {
+        namespace vs = vislib;
+        namespace gy = vs::gyro;
+    };
+    
+    class Gyroscope;
+
+};
+
+class vislib_mpu6050::Gyroscope : public vislib::gyro::UltimateGyroController<double, double, size_t>, public MPU6050 {
+public:
+    inline vislib::util::Result<vislib::util::Vector<double>> getAcceleration() const noexcept override {
+        vs::util::Vector<double> acceleration(vs::util::Array<double>({double(), double(), double()}));
+
+        int16_t ax, ay, az;
+
+        MPU6050::getAcceleration(&ax, &ay, &az);
+
+        acceleration[0] = ax / 16384.;
+        acceleration[1] = ay / 16384.;
+        acceleration[2] = az / 16384.;
+
+        return acceleration;
+
+    }
+
+    inline vislib::util::Result<vislib::util::Vector<double>> getAngularSpeed() const noexcept override {
+        vs::util::Vector<double> speed(vs::util::Array<double>({double(), double(), double()}));
+
+        int16_t gx, gy, gz;
+
+        MPU6050::getRotation(&gx, &gy, &gz);
+
+        speed[0] = gx / 131.;
+        speed[1] = gy / 131.;
+        speed[2] = gz / 131.;
+
+        return speed;
+
+    }
+};
