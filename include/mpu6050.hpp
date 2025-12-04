86d4082b3c6602bfd4544fcae17789b75f16ca40
allivka <allivka1920@gmail.com>
2025-12-04 23:50:28 +0300

    Undone patch, work in progress

--- include/mpu6050.hpp
+++ include/mpu6050.hpp
@@ -1,22 +1,66 @@
 #pragma once
 
 #include <vislib.hpp>
-#include <MPU6050.h>
+#include <MPU6050_6Axis_MotionApps20.h>
 
 namespace vislib_mpu6050 {
-    class GyroscopeDMP;
+    class GyroscopeBase;
+    template <size_t InterruptRegistrySize = 1> class GyroscopeDMP;
     class GyroscopeCalculator;
 };
 
-class vislib_mpu6050::GyroscopeDMP : public vislib::gyro::UltimateGyroGetter<double, size_t>, public MPU6050 {
-    
+class vislib_mpu6050::GyroscopeBase : public vislib::gyro::UltimateGyroGetter<double, size_t>, public MPU6050 {
+
+public:
+
+    virtual ~GyroscopeBase() = default;
+
+    virtual inline vislib::util::Error init() noexcept {
+        initialize();
+        if (!testConnection()) return {vislib::util::ErrorCode::initFailed, "Failed mpu6050 connection test"};
+        calibrate();
+
+        return {};
+    }
+
+    virtual vislib::util::Error calibrate() noexcept override {
+
+        this->setXGyroOffset(0);
+        this->setYGyroOffset(0);
+        this->setZGyroOffset(0);
+
+        this->setXAccelOffset(0);
+        this->setYAccelOffset(0);
+        this->setZAccelOffset(0);
+        
+        this->CalibrateAccel(6);
+        this->CalibrateGyro(6);
+        
+        return {};
+    }
 };
 
-class vislib_mpu6050::GyroscopeCalculator : public vislib::gyro::UltimateGyroCalculator<double, size_t>, public MPU6050 {
+class vislib_mpu6050::GyroscopeCalculator : public vislib_mpu6050::GyroscopeBase, public vislib::gyro::UltimateGyroCalculator<double, size_t> {
 protected:
     double gyroDivider = 131.0;
     double accelerationDivider = 16384.0;
 public:
+
+    virtual vislib::util::Error calibrate() noexcept override {
+
+        static_cast<vislib_mpu6050::GyroscopeBase*>(this)->calibrate();
+
+        yawConfig.offset = 0;
+        yawConfig.integrator.setIntegral(0);
+
+        pitchConfig.offset = 0;
+        pitchConfig.integrator.setIntegral(0);
+
+        rollConfig.offset = 0;
+        rollConfig.integrator.setIntegral(0);
+
+        return {};
+    }
 
     virtual inline vislib::util::Result<vislib::util::Vector<double>> getAcceleration() const noexcept final {
         vislib::util::Vector<double> acceleration(vislib::util::Array<double>({double(), double(), double()}));
@@ -39,51 +83,53 @@ public:
         int16_t gx, gy, gz;
 
         static_cast<MPU6050>(*this).getRotation(&gx, &gy, &gz);
-        
+
         speed[0] = gx / gyroDivider;
         speed[1] = gy / gyroDivider;
-        speed[2] = gz / gyroDivider;        
-        
+        speed[2] = gz / gyroDivider;
+
         return speed;
 
     }
-    
-    virtual vislib::util::Error calibrate() noexcept override {
-        
-        
-        this->setXGyroOffset(0);
-        this->setYGyroOffset(0);
-        this->setZGyroOffset(0);
-        
-        this->setXAccelOffset(0);
-        this->setYAccelOffset(0);
-        this->setZAccelOffset(0);
-        
-        this->CalibrateAccel();
-        this->CalibrateGyro();
-        
-        yawConfig.offset = 0;
-        yawConfig.integrator.setIntegral(0);
-        
-        pitchConfig.offset = 0;
-        pitchConfig.integrator.setIntegral(0);
-        
-        rollConfig.offset = 0;
-        rollConfig.integrator.setIntegral(0);
-        
-        return {};
-    }
-    
+
     virtual inline vislib::util::Error setGyroDivider(double divider) noexcept {
         if (divider == 0) return {vislib::util::ErrorCode::invalidArgument, "Cannot set gyro divider to zero"};
         gyroDivider = divider;
         return {};
     }
-    
+
     virtual inline vislib::util::Error setAccelerationDivider(double divider) noexcept {
         if (divider == 0) return {vislib::util::ErrorCode::invalidArgument, "Cannot set acceleration divider to zero"};
         accelerationDivider = divider;
         return {};
     }
-    
+
+    virtual ~GyroscopeCalculator() = default;
+
+};
+
+ class vislib_mpu6050::GyroscopeDMP : public vislib_mpu6050::GyroscopeBase {
+protected:
+
+    volatile bool dmpReady = false;
+
+public:
+
+     virtual vislib::util::Error initDMP(uint16_t interruptPin) noexcept {
+        uint8_t status = dmpInitialize();
+
+        if (status != 0) return {vislib::util::ErrorCode::initFailed,
+            "DMP initialization failed, status code = " + vislib::util::to_string(static_cast<size_t>(status))
+        };
+
+        pinMode(interruptPin, INPUT);
+        calibrate();
+        setDMPEnabled(true);
+
+        attachInterrupt(digitalPinToInterrupt(interruptPin), [this] {this->dmpReady = true;}, RISING);
+        dmpReady = true;
+
+
+    }
+
 };
---
+++ include/temp.h
@@ -0,0 +1,128 @@
+
+#include "I2Cdev.h"
+#include "MPU6050_6Axis_MotionApps20.h"
+
+MPU6050 mpu;
+
+int const INTERRUPT_PIN = 2;  // Define the interruption #0 pin
+bool blinkState;
+
+/*---MPU6050 Control/Status Variables---*/
+bool DMPReady = false;  // Set true if DMP init was successful
+uint8_t MPUIntStatus;   // Holds actual interrupt status byte from MPU
+uint8_t devStatus;      // Return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // Expected DMP packet size (default is 42 bytes)
+uint8_t FIFOBuffer[64]; // FIFO storage buffer
+
+/*---Orientation/Motion Variables---*/
+Quaternion q;           // [w, x, y, z]         Quaternion container
+VectorInt16 aa;         // [x, y, z]            Accel sensor measurements
+VectorInt16 gy;         // [x, y, z]            Gyro sensor measurements
+VectorInt16 aaReal;     // [x, y, z]            Gravity-free accel sensor measurements
+VectorInt16 aaWorld;    // [x, y, z]            World-frame accel sensor measurements
+VectorFloat gravity;    // [x, y, z]            Gravity vector
+float euler[3];         // [psi, theta, phi]    Euler angle container
+float ypr[3];           // [yaw, pitch, roll]   Yaw/Pitch/Roll container and gravity vector
+
+/*-Packet structure for InvenSense teapot demo-*/
+uint8_t teapotPacket[14] = { '$', 0x02, 0, 0, 0, 0, 0, 0, 0, 0, 0x00, 0x00, '\r', '\n' };
+
+volatile bool MPUInterrupt = false;
+void DMPDataReady() {
+  MPUInterrupt = true;
+}
+
+void setup() {
+  mpu.initialize();
+  pinMode(INTERRUPT_PIN, INPUT);
+
+  Serial.println(F("Testing MPU6050 connection..."));
+  if(mpu.testConnection() == false){
+    Serial.println("MPU6050 connection failed");
+    while(true);
+  }
+  else {
+    Serial.println("MPU6050 connection successful");
+  }
+
+  Serial.println(F("\nSend any character to begin: "));
+  while (Serial.available() && Serial.read());
+  while (!Serial.available());
+  while (Serial.available() && Serial.read());
+
+  Serial.println(F("Initializing DMP..."));
+  devStatus = mpu.dmpInitialize();
+
+  mpu.setXGyroOffset(0);
+  mpu.setYGyroOffset(0);
+  mpu.setZGyroOffset(0);
+  mpu.setXAccelOffset(0);
+  mpu.setYAccelOffset(0);
+  mpu.setZAccelOffset(0);
+
+  if (devStatus == 0) {
+    mpu.CalibrateAccel(6);
+    mpu.CalibrateGyro(6);
+    Serial.println("These are the Active offsets: ");
+    mpu.PrintActiveOffsets();
+    Serial.println(F("Enabling DMP..."));
+    mpu.setDMPEnabled(true);
+
+    Serial.print(F("Enabling interrupt detection (Arduino external interrupt "));
+    Serial.print(digitalPinToInterrupt(INTERRUPT_PIN));
+    Serial.println(F(")..."));
+    attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), DMPDataReady, RISING);
+    MPUIntStatus = mpu.getIntStatus();
+
+    Serial.println(F("DMP ready! Waiting for first interrupt..."));
+    DMPReady = true;
+    packetSize = mpu.dmpGetFIFOPacketSize();
+  }
+  else {
+    Serial.print(F("DMP Initialization failed (code "));
+    Serial.print(devStatus);
+    Serial.println(F(")"));
+  }
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  if (!DMPReady) return;
+
+  if (mpu.dmpGetCurrentFIFOPacket(FIFOBuffer)) {
+
+    mpu.dmpGetQuaternion(&q, FIFOBuffer);
+    mpu.dmpGetGravity(&gravity, &q);
+    mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+    Serial.print("ypr\t");
+    Serial.print(ypr[0] * 180/M_PI);
+    Serial.print("\t");
+    Serial.print(ypr[1] * 180/M_PI);
+    Serial.print("\t");
+    Serial.println(ypr[2] * 180/M_PI);
+
+    mpu.dmpGetQuaternion(&q, FIFOBuffer);
+    mpu.dmpGetAccel(&aa, FIFOBuffer);
+    mpu.dmpGetGravity(&gravity, &q);
+    mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
+    Serial.print("areal\t");
+    Serial.print(aaReal.x);
+    Serial.print("\t");
+    Serial.print(aaReal.y);
+    Serial.print("\t");
+    Serial.println(aaReal.z);
+
+
+    mpu.dmpGetQuaternion(&q, FIFOBuffer);
+    mpu.dmpGetAccel(&aa, FIFOBuffer);
+    mpu.dmpGetGravity(&gravity, &q);
+    mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
+    mpu.dmpGetLinearAccelInWorld(&aaWorld, &aaReal, &q);
+    Serial.print("aworld\t");
+    Serial.print(aaWorld.x);
+    Serial.print("\t");
+    Serial.print(aaWorld.y);
+    Serial.print("\t");
+    Serial.println(aaWorld.z);
+  }
+}
